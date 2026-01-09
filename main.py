6b141bab0a8b12250de35ba5243bd9a8d507012e
Vero2356 <p.bravnik@email.cz>
2026-01-09 22:16:18 +0100

    Add detailed CPU information and boot time formatting to main.py

--- main.py
+++ main.py
@@ -28,4 +28,23 @@ print(f"CPU: {uname.processor}")
 print("="*40, "Boot Time", "="*40)
 boot_time_timestamp = psutil.boot_time()
 bt = datetime.fromtimestamp(boot_time_timestamp)
-print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+print(f"Boot Time: {bt.day}/{bt.month}/{bt.year} {bt.hour}:{bt.minute}:{bt.second}")
+
+# printing information about CPU like cores, threads, freq., etc.
+
+print("="*40, "CPU Info", "="*40)
+# cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+
+# frequency
+cpufreq = psutil.cpu_freq()
+print(f"Max frequency {cpufreq.max:.02f}Mhz")
+print(f"Min frequency {cpufreq.min:.02f}Mhz")
+print(f"current frequency {cpufreq.current:.02f}Mhz")
+
+# usage
+print("CPU Usage per Core")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+    print(f"Total CPU Usage: {psutil.cpu_percent()}%")
